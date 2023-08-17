795e6ec0fb3643e38e510d572a359022bff27da0
Yaroslav Vendysh <yaroslav.vendysh@spacewell.com>
2023-08-17 20:52:14 +0300

    10 commit

--- src/Html5QrcodePlugin.jsx
+++ src/Html5QrcodePlugin.jsx
@@ -7,45 +7,22 @@ const config = {
   fps: 10,
   qrbox: 500,
   disableFlip: false,
-  videoConstraints: { facingMode: { exact: "environment" } },
 };
 
 export const Html5QrcodePlugin = () => {
   const instance = useRef();
   const [data, setData] = useState("");
-  const [cameras, setCameras] = useState([]);
-  const [camera, setCamera] = useState(null);
 
   const onHtml5QrcodeResult = useCallback((decodedText, decodedResult) => {
     setData(decodedText);
     console.log("Result onHtml5Qrcode", decodedResult);
   }, []);
 
-  const onFlip = useCallback(() => {
-    const index = cameras.findIndex((prevCam) => prevCam.id === camera)[0];
-    const newCameraId = cameras[index + 1 < cameras.length ? index + 1 : 0].id;
-
-    instance.current?.stop();
-
-    instance.current
-      .start(newCameraId, config, onHtml5QrcodeResult)
-      .catch((error) => console.log(error));
-  }, [cameras, camera]);
-
   useEffect(() => {
     instance.current = new Html5Qrcode(QrCodeScannerContainerId);
 
-    Html5Qrcode.getCameras()
-      .then((devices) => {
-        if (devices && devices.length) {
-          const cameraId = devices[0].id;
-
-          setCameras(devices);
-          setCamera(cameraId);
-
-          instance.current.start(cameraId, config, onHtml5QrcodeResult);
-        }
-      })
+    instance.current
+      .start({ facingMode: "environment" }, config, onHtml5QrcodeResult)
       .catch((error) => console.log(error));
   }, []);
 
@@ -60,15 +37,8 @@ export const Html5QrcodePlugin = () => {
 
   return (
     <div className="approach">
-      <div>
-        Html5-QRCode - {data} <br />
-        Devices - {cameras.map(({ label }) => label).join(";")} <br />
-        Current device -{" "}
-        {cameras.find((prevCam) => prevCam.id === camera)?.label}
-      </div>
+      <div>Html5-QRCode - {data}</div>
       <div id={QrCodeScannerContainerId} className="qr-code-read-container" />
-      <button onClick={onFlip}>FLIP</button>
-      {/*{cameras.length > 1 && <button onClick={onFlip}>FLIP</button>}*/}
     </div>
   );
 };
