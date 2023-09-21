db3529c20fb7913a4af6c382b7b09d859195b741
Yaroslav Vendysh <yaroslav.vendysh@spacewell.com>
2023-09-21 11:01:17 +0300

    Test messages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { Html5QrcodePlugin } from "./Html5QrcodePlugin";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Html5QrcodePlugin />
-    </div>
-  );
+  return <div className="App">{/*<Html5QrcodePlugin />*/}</div>;
 };
 
 export default App;
